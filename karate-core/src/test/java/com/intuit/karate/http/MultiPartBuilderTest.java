e267155a81562554608f3e831d79d1b3333a2ad0
Tim Bergbom <tbergbom@kth.se>
2026-02-20 10:48:32 +0100

    Add two unit tests to MultiPartBuilder

--- karate-core/src/test/java/com/intuit/karate/http/MultiPartBuilderTest.java
+++ karate-core/src/test/java/com/intuit/karate/http/MultiPartBuilderTest.java
@@ -3,6 +3,9 @@ package com.intuit.karate.http;
 import com.intuit.karate.FileUtils;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -54,4 +57,55 @@ class MultiPartBuilderTest {
         assertEquals("bar=hello+world", actual);
     }
 
+	@Test
+	void testList() {
+		MultiPartBuilder builder = new MultiPartBuilder(true, null);
+		Map<String, Object> input = new HashMap<String, Object>();
+		input.put("name", "bar");
+		input.put("value", Arrays.asList("one", "two", "three"));
+		builder.part(input);
+		byte[] bytes = builder.build();
+		String boundary = builder.getBoundary();
+		String actual = FileUtils.toString(bytes);
+		String expected = join(
+			"--" + boundary,
+			"content-disposition: form-data; name=\"bar\"",
+			"content-length: 3",
+			"content-type: text/plain",
+			"",
+			"one",
+			"--" + boundary,
+			"content-disposition: form-data; name=\"bar\"",
+			"content-length: 3",
+			"content-type: text/plain",
+			"",
+			"two",
+			"--" + boundary,
+			"content-disposition: form-data; name=\"bar\"",
+			"content-length: 5",
+			"content-type: text/plain",
+			"",
+			"three",
+			"--" + boundary + "--"
+		);
+		assertEquals("multipart/form-data; boundary=" + boundary, builder.getContentTypeHeader());
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testNullValue() {
+		MultiPartBuilder builder = new MultiPartBuilder(true, null);
+		builder.part("name", null);
+		byte[] bytes = builder.build();
+        String actual = FileUtils.toString(bytes);
+		String boundary = builder.getBoundary();
+		String expected = join(
+			"--" + boundary,
+			"content-disposition: form-data; name=\"name\"",
+			"content-length: 0",
+			"content-type: application/octet-stream",
+			"--" + boundary + "--"
+		);
+		assertEquals(expected, actual);
+	}
 }
